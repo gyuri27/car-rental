b5bf7ac07041c90c64d62b5a609163e79ae2a5dc
gyuri27 <gyurka432@gmail.com>
2023-05-25 13:15:12 +0200

    Workflow_v5 javitas

--- src/main/java/com/example/car/rental/web/controller/handler/Car_rentalExceptionHandler.java
+++ src/main/java/com/example/car/rental/web/controller/handler/Car_rentalExceptionHandler.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
 /**
